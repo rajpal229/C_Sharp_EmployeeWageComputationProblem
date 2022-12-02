c74ae29c26f3e433b4c77ebac2cbe1eb4e7db9bb
rajpal229 <rajpalrawat229@gmail.com>
2022-12-02 21:28:31 +0530

    Added UC6_CalculateWagesTillConditionOfTotalWorkingHoursOrDaysReached

--- Program.cs
+++ Program.cs
@@ -4,8 +4,8 @@
     {
         static void Main(string[] args)
         {
-            int fullTime = 1, partTime = 0, emp_Rate_Per_Hr = 20, empHrs = 0, empWage = 0, totalSalary = 0, numWorkingDays = 20;
-            for (int day = 1; day <= numWorkingDays; day++)
+            int fullTime = 1, partTime = 0, emp_Rate_Per_Hr = 20, empHrs = 0, empWage = 0, totalSalary = 0, numWorkingDays = 20,day=1,totalWorkingHours=0;
+            while (day <= numWorkingDays && totalWorkingHours <= 100)
             {
                 Random rannum = new Random();
                 int checkPresent = rannum.Next(0, 3);
@@ -25,11 +25,18 @@
                         Console.WriteLine("Employee is Absent");
                         break;
                 }
+                totalWorkingHours = totalWorkingHours + empHrs;
+                if (totalWorkingHours > 100)
+                {
+                    totalWorkingHours = totalWorkingHours - empHrs;
+                    break;
+                }
                 empWage = empHrs * emp_Rate_Per_Hr;
                 Console.WriteLine("Employee Wage of Day" + day + " :" + empWage);
                 totalSalary = totalSalary + empWage;
-                Console.WriteLine("Total Salary of a Month: " + totalSalary);
+                day++;
             }
+            Console.WriteLine("Total Salary of a Month: " + totalSalary);
         }
     }
 }
