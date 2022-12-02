23e6c61a3fc060b50c01ff34495e65d4605c4d9e
rajpal229 <rajpalrawat229@gmail.com>
2022-12-02 18:59:09 +0530

    Added: UC3_AddedPartTime

--- Program.cs
+++ Program.cs
@@ -4,22 +4,27 @@
     {
         static void Main(string[] args)
         {
-            int fullTime = 1, emp_Rate_Per_Hr = 20, empHrs = 0, empWage = 0;
+            int fullTime = 1, partTime = 0, emp_Rate_Per_Hr = 20, empHrs = 0, empWage = 0;
             Random rannum = new Random();
-            int checkPresent = rannum.Next(0, 2);
+            int checkPresent = rannum.Next(0, 3);
             Console.WriteLine("Random Number: " + checkPresent);
-            if (checkPresent == fullTime)
+            if (checkPresent == 1)
             {
                 empHrs = 8;
-                Console.WriteLine("Employee is Present");
+                Console.WriteLine("Employee is Present Full TIme");
+            }
+            else if (checkPresent == 2)
+            {
+                empHrs = 4;
+                Console.WriteLine("Employee is Present Full TIme");
             }
             else
             {
                 empHrs = 0;
                 Console.WriteLine("Employee is Absent");
             }
-            empWage = empHrs * emp_Rate_Per_Hr;
-            Console.WriteLine("Employee Wage: " + empWage);
+        empWage = empHrs * emp_Rate_Per_Hr;
+        Console.WriteLine("Employee Wage: " + empWage);
         }
     }
 }
