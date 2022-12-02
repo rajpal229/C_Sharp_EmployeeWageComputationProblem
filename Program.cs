613f27963ab2cb603d7abb5146a4c55f64319084
rajpal229 <rajpalrawat229@gmail.com>
2022-12-02 18:41:08 +0530

    Welcome to Employee Wage Computation Problem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmployeeWageComputationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+        }
+    }
+}
