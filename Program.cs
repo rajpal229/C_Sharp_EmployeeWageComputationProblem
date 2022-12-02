4fcde1e501a58291ada37ab524fc568dacde7030
rajpal229 <rajpalrawat229@gmail.com>
2022-12-02 18:43:23 +0530

    Added: UC1_EmployeeAttendance

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Problem");
+            int fullTime = 1;
+            Random rannum = new Random();
+            int checkPresent = rannum.Next(0, 2);
+            Console.WriteLine("Random Number: " + checkPresent);
+            if (checkPresent == fullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
